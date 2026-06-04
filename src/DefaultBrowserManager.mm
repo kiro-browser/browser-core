308cdc258e60c8a65a033570ba88f537ccdb2c63
allexanderbergmans <allexanderbergmans@student.elisa.be>
2026-06-04 18:13:45 +0200

    * Fixes

--- src/DefaultBrowserManager.mm
+++ src/DefaultBrowserManager.mm
@@ -4,6 +4,12 @@
 
 @implementation DefaultBrowserManager
 
++ (NSError*)errorWithMessage:(NSString*)message code:(NSInteger)code details:(NSString*)details {
+    NSMutableDictionary* userInfo = [@{ NSLocalizedDescriptionKey: message } mutableCopy];
+    if (details.length) userInfo[NSLocalizedRecoverySuggestionErrorKey] = details;
+    return [NSError errorWithDomain:@"BuildBrowserDefaultBrowser" code:code userInfo:userInfo];
+}
+
 + (BOOL)isDefaultBrowser {
     NSString* bundleID = NSBundle.mainBundle.bundleIdentifier;
     if (!bundleID.length) return NO;
@@ -18,10 +24,16 @@
 + (BOOL)setAsDefaultBrowserWithError:(NSError**)error {
     NSString* bundleID = NSBundle.mainBundle.bundleIdentifier;
     if (!bundleID.length) {
+        if (error) *error = [self errorWithMessage:@"BuildBrowser does not have a bundle identifier." code:-1 details:nil];
+        return NO;
+    }
+
+    NSURL* bundleURL = NSBundle.mainBundle.bundleURL;
+    OSStatus registerStatus = bundleURL ? LSRegisterURL((__bridge CFURLRef)bundleURL, true) : -1;
+    if (registerStatus != noErr) {
         if (error) {
-            *error = [NSError errorWithDomain:@"BuildBrowserDefaultBrowser"
-                                         code:-1
-                                     userInfo:@{ NSLocalizedDescriptionKey: @"BuildBrowser does not have a bundle identifier." }];
+            NSString* details = [NSString stringWithFormat:@"Launch Services could not register %@. OSStatus: %d", bundleURL.path ?: @"the app bundle", (int)registerStatus];
+            *error = [self errorWithMessage:@"macOS could not register BuildBrowser as an app that can open links." code:(NSInteger)registerStatus details:details];
         }
         return NO;
     }
@@ -31,9 +43,8 @@
     if (httpStatus == noErr && httpsStatus == noErr) return YES;
 
     if (error) {
-        *error = [NSError errorWithDomain:@"BuildBrowserDefaultBrowser"
-                                     code:(NSInteger)(httpStatus != noErr ? httpStatus : httpsStatus)
-                                 userInfo:@{ NSLocalizedDescriptionKey: @"macOS could not set BuildBrowser as the default browser." }];
+        NSString* details = [NSString stringWithFormat:@"Bundle ID: %@. http OSStatus: %d. https OSStatus: %d.", bundleID, (int)httpStatus, (int)httpsStatus];
+        *error = [self errorWithMessage:@"macOS could not set BuildBrowser as the default browser." code:(NSInteger)(httpStatus != noErr ? httpStatus : httpsStatus) details:details];
     }
     return NO;
 }
